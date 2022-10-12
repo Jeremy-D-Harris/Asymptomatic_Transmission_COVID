b98994321b45ec4971d43958234eca74d977cc7c
Jeremy Harris <jeremy.harris31@gmail.com>
2022-10-11 21:05:02 -0400

    extra sims

--- Code_plt_ms_figures/main_plt_Fig1_SEIR_fixedpropasymp_twodiseases_sameR0s_031022.m
+++ Code_plt_ms_figures/main_plt_Fig1_SEIR_fixedpropasymp_twodiseases_sameR0s_031022.m
@@ -10,11 +10,12 @@ save_ans_Fig = 0;
 % 0: don't save
 % 1: save
 
+% figure_name = 'Figure1_fixedpropasymp_sameR0s_100522_withextrasim';
 figure_name = 'Figure1_fixedpropasymp_sameR0s_031022';
 
 %% load no mitigation files
 
-switch_over_Ta = [5,6,8];
+switch_over_Ta = [5,6,8,9]; % note that 9 corresponds to Ta=8,Ts=2days
 
 frac_spacing = 0.74;
 frac_scaling = 0.2;
@@ -52,6 +53,14 @@ for counter=1:length(switch_over_Ta)
             gamma_s3 = params.gamma_s;
             gamma_a3 = params.gamma_a;
             
+        case 9
+            % load: Ta=8
+            infile = 'SEIR_fixedpropasymp_twodiseases_sameR0s_100522_T2and8.mat';
+            load(strcat('./sim_data/',infile));
+            cbf_colors = [0,0,0]/255; % black
+            gamma_s4 = params.gamma_s;
+            gamma_a4 = params.gamma_a;
+            
     end
     
     fprintf('Opened file: \n'); % want to be close to 25 days in
@@ -59,19 +68,19 @@ for counter=1:length(switch_over_Ta)
     
     fprintf('Infectious periods: \n');
     fprintf('T_a = %1i days \n',1/params.gamma_a);
-    fprintf('T_s = %1i days \n\n',1/params.gamma_s); 
+    fprintf('T_s = %1i days \n\n',1/params.gamma_s);
     fprintf('-------------------------------------- \n\n');
-   
+    
     
     %% plot first column - no mitigation
     
     f1 = figure(1); set(f1, 'Position', [100 500 800 650]);
-    subplot(4,2,1); 
+    subplot(4,2,1);
     this_p = semilogy(params.t_span, results.total_incidence,'Color',cbf_colors,'LineWidth',2); hold on;
     this_p.Color(4) = 1-0.18*(counter);
     axis([0 params.t_span(end) 10^(-6) 1]);
     
-%     xlabel('Time (days)');
+    %     xlabel('Time (days)');
     ylabel({'Total'; 'Incidence, $i(t)$'},'Interpreter','Latex');
     f1=gca;
     f1.LineWidth = 1;
@@ -81,7 +90,7 @@ for counter=1:length(switch_over_Ta)
     
     title('Susceptible Depletion','FontSize',16);
     
-    if counter==3
+    if counter==4
         txt = {'A'};
         text(0.025,0.925,txt,'Units','normalized','FontSize',14,'FontWeight','bold');
         
@@ -101,13 +110,13 @@ for counter=1:length(switch_over_Ta)
     
     
     %%
-    figure(1); 
-    subplot(4,2,3); 
+    figure(1);
+    subplot(4,2,3);
     this_p=plot(params.t_span, results.proportion_asymp_transmission,'Color',cbf_colors,'LineWidth',2); hold on;
     this_p.Color(4) = 1-0.18*(counter);
     
     axis([0 params.t_span(end) 0 1]);
-%     xlabel('Time (days)');
+    %     xlabel('Time (days)');
     ylabel({'Proportion'; 'Asymptomatic'; 'Transmission, $q(t)$'},'Interpreter','Latex');
     f1=gca;
     f1.LineWidth = 1;
@@ -116,7 +125,7 @@ for counter=1:length(switch_over_Ta)
     f1.FontName = 'Times';
     
     
-    if counter==3
+    if counter==4
         txt = {'B'};
         text(0.025,0.925,txt,'Units','normalized','FontSize',14,'FontWeight','bold');
         
@@ -130,7 +139,7 @@ for counter=1:length(switch_over_Ta)
         text(-0.045,0.93,txt,'Units','normalized',...
             'FontSize',14,'FontWeight','normal','FontName', 'Times');
         
-                txt = {'0'};
+        txt = {'0'};
         text(-0.045,0.045,txt,'Units','normalized',...
             'FontSize',14,'FontWeight','normal','FontName', 'Times');
         
@@ -138,13 +147,13 @@ for counter=1:length(switch_over_Ta)
     
     %%
     
-%     pause;
+    %     pause;
     figure(1); subplot(4,2,5);
     this_p = plot(params.t_span, results.proportion_asymp_incidence,'Color',cbf_colors,'LineWidth',2); hold on;
     this_p.Color(4) = 1-0.18*(counter);
     
     axis([0 params.t_span(end) 0 1]);
-%     xlabel('Time (days)');
+    %     xlabel('Time (days)');
     ylabel({'Proportion'; 'Asymptomatic'; 'Incidence, $p(t)$'},'Interpreter','Latex');
     f1=gca;
     f1.LineWidth = 1;
@@ -153,7 +162,7 @@ for counter=1:length(switch_over_Ta)
     f1.FontName = 'Times';
     
     
-    if counter==3
+    if counter==4
         txt = {'C'};
         text(0.025,0.925,txt,'Units','normalized','FontSize',14,'FontWeight','bold');
         
@@ -167,15 +176,15 @@ for counter=1:length(switch_over_Ta)
         text(-0.045,0.93,txt,'Units','normalized',...
             'FontSize',14,'FontWeight','normal','FontName', 'Times');
         
-                txt = {'0'};
+        txt = {'0'};
         text(-0.045,0.045,txt,'Units','normalized',...
             'FontSize',14,'FontWeight','normal','FontName', 'Times');
         
     end
     
-
     
-%     pause;
+    
+    %     pause;
     figure(1); subplot(4,2,7);
     %     plot(params.t_span,Rt_fixedpropasymp,'Color',cbf_colors,'LineWidth',2); hold on;
     semilogy(params.t_span,ones(size(params.t_span)),'k','LineWidth',0.5); hold on;
@@ -191,16 +200,16 @@ for counter=1:length(switch_over_Ta)
     f1.FontSize = 14;
     f1.FontWeight = 'normal';
     f1.FontName = 'Times';
-%     this_p.Color(4) = 1-0.18*(counter);
+    %     this_p.Color(4) = 1-0.18*(counter);
     
     
-    if counter==3
+    if counter==4
         txt = {'D'};
         text(0.025,0.925,txt,'Units','normalized','FontSize',14,'FontWeight','bold');
         
         set(f1,'Position',[old_pos(1), old_pos(2)-frac_scaling, old_pos(3), old_pos(4)])
         old_pos = get(f1, 'Position');
-
+        
         set(f1,'yticklabel',[{'0.25'},{'0.5'},{'1'},{'2'},{''},{''}]);
         set(f1,'YminorTick','off');
         box('off');
@@ -252,6 +261,14 @@ for counter=1:length(switch_over_Ta)
             gamma_s3 = params.gamma_s;
             gamma_a3 = params.gamma_a;
             
+        case 9
+            % load: Ta=8
+            infile = 'SEIR_fixedpropasymp_twodiseases_sameR0s_100522_T2and8_mit.mat';
+            load(strcat('./sim_data/',infile));
+            cbf_colors = [0,0,0]/255; % light blue
+            gamma_s4 = params.gamma_s;
+            gamma_a4 = params.gamma_a;
+            
     end
     
     fprintf('Opened file: \n'); % want to be close to 25 days in
@@ -267,10 +284,12 @@ for counter=1:length(switch_over_Ta)
     figure(1);
     subplot(4,2,2);
     h(counter) = semilogy(params.t_span, results.total_incidence,'Color',cbf_colors,'LineWidth',2); hold on;
+    %     if counter~=4
     h(counter).Color(4) = 1-0.18*(counter); % transparency
+    %     end
     
     axis([0 params.t_span(end) 10^(-6) 1]);
-%     xlabel('Time (days)');
+    %     xlabel('Time (days)');
     ylabel({'Total'; 'Incidence, $i(t)$'},'Interpreter','Latex');
     
     f1=gca;
@@ -281,7 +300,7 @@ for counter=1:length(switch_over_Ta)
     
     title('Intervention','FontSize',16);
     
-    if counter==3
+    if counter==4
         txt = {'E'};
         text(0.025,0.925,txt,'Units','normalized','FontSize',14,'FontWeight','bold');
         
@@ -298,23 +317,24 @@ for counter=1:length(switch_over_Ta)
             'FontSize',14,'FontWeight','normal','FontName', 'Times');
     end
     
-    if counter==3
+    if counter==4
         
         legend_char1 = ['$T_s = T_a = ', num2str(1/gamma_a1),'$'];
         legend_char2 = ['$T_s = ', num2str(1/gamma_s2),'$, $T_a = ', num2str(1/gamma_a2),'$'];
         legend_char3 = ['$T_s = ', num2str(1/gamma_s3),'$, $T_a = ', num2str(1/gamma_a3),'$'];
-        legend(h,{legend_char1,legend_char2,legend_char3}, 'Interpreter','Latex','Location','NorthEast','FontSize',12);
+        legend_char4 = ['$T_s = ', num2str(1/gamma_s4),'$, $T_a = ', num2str(1/gamma_a4),'$'];
+        legend(h,{legend_char1,legend_char2,legend_char3,legend_char4}, 'Interpreter','Latex','Location','NorthEast','FontSize',11);
         
         legend boxoff
     end
     
-   
+    
     figure(1); subplot(4,2,4);
     this_p=plot(params.t_span, results.proportion_asymp_transmission,'Color',cbf_colors,'LineWidth',2); hold on;
     this_p.Color(4) = 1-0.18*(counter);
     
     axis([0 params.t_span(end) 0 1]);
-%     xlabel('Time (days)');
+    %     xlabel('Time (days)');
     ylabel({'Proportion'; 'Asymptomatic'; 'Transmission, $q(t)$'},'Interpreter','Latex');
     f1=gca;
     f1.LineWidth = 1;
@@ -324,7 +344,7 @@ for counter=1:length(switch_over_Ta)
     
     
     
-if counter==3
+    if counter==4
         txt = {'F'};
         text(0.025,0.925,txt,'Units','normalized','FontSize',14,'FontWeight','bold');
         
@@ -338,7 +358,7 @@ if counter==3
         text(-0.045,0.93,txt,'Units','normalized',...
             'FontSize',14,'FontWeight','normal','FontName', 'Times');
         
-                txt = {'0'};
+        txt = {'0'};
         text(-0.045,0.045,txt,'Units','normalized',...
             'FontSize',14,'FontWeight','normal','FontName', 'Times');
         
@@ -349,7 +369,7 @@ if counter==3
     this_p=plot(params.t_span, results.proportion_asymp_incidence,'Color',cbf_colors,'LineWidth',2); hold on;
     this_p.Color(4) = 1-0.18*(counter);
     axis([0 params.t_span(end) 0 1]);
-%     xlabel('Time (days)');
+    %     xlabel('Time (days)');
     ylabel({'Proportion'; 'Asymptomatic'; 'Incidence, $p(t)$'},'Interpreter','Latex');
     f1=gca;
     f1.LineWidth = 1;
@@ -358,7 +378,7 @@ if counter==3
     f1.FontName = 'Times';
     
     
-if counter==3
+    if counter==4
         txt = {'G'};
         text(0.025,0.925,txt,'Units','normalized','FontSize',14,'FontWeight','bold');
         
@@ -372,7 +392,7 @@ if counter==3
         text(-0.045,0.93,txt,'Units','normalized',...
             'FontSize',14,'FontWeight','normal','FontName', 'Times');
         
-                txt = {'0'};
+        txt = {'0'};
         text(-0.045,0.045,txt,'Units','normalized',...
             'FontSize',14,'FontWeight','normal','FontName', 'Times');
         
@@ -392,13 +412,13 @@ if counter==3
     f1.FontWeight = 'normal';
     f1.FontName = 'Times';
     
-    if counter==3
+    if counter==4
         txt = {'H'};
         text(0.025,0.925,txt,'Units','normalized','FontSize',14,'FontWeight','bold');
         
         set(f1,'Position',[old_pos(1), old_pos(2)-frac_scaling, old_pos(3), old_pos(4)])
         old_pos = get(f1, 'Position');
-
+        
         set(f1,'yticklabel',[{'0.25'},{'0.5'},{'1'},{'2'},{''},{''}]);
         set(f1,'YminorTick','off');
         box('off');
@@ -414,16 +434,16 @@ end
 %% save figure
 if save_ans_Fig
     
-    folder_location = './../Figures_ms_all/main/';
+    folder_location = './../Figures_ms_all/extra_figs/';
     saveas(f1,strcat(folder_location,figure_name),'epsc');
     
-    fprintf('Figure saved:\n'); 
+    fprintf('Figure saved:\n');
     fprintf(strcat(figure_name,'\n\n'));
     
-    fprintf('Location:\n'); 
+    fprintf('Location:\n');
     fprintf(strcat(folder_location,'\n\n'));
     
-    else
+else
     
     fprintf('Figure not saved.\n');
     
